6bb845df84ef0dd7a746f37cf288d3f708b46ceb
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-08-03 20:12:23 +0900

    [追加]ビルド設定を追加

--- scripts/build.js
+++ scripts/build.js
@@ -1,10 +1,11 @@
 const esbuild = require("esbuild");
+const isDevelopment = process.env.MODE === "development";
 
 esbuild.build({
     entryPoints: ['./src/main.ts'],
     bundle: true,
-    minify: false,
+    minify: !isDevelopment,
     sourcemap: "inline",
-    outdir: 'G:\\Projects\\C#\\06_niconicome\\Niconicome\\Niconicome\\bin\\Debug\\net5.0-windows10.0.19041.0\\win-x64\\addons\\5147f547-c273-411c-a8ae-49d4fc6ffa7e\\scripts',
+    outdir: isDevelopment ? 'G:\\Projects\\C#\\06_niconicome\\Niconicome\\Niconicome\\bin\\Debug\\net5.0-windows10.0.19041.0\\win-x64\\addons\\5147f547-c273-411c-a8ae-49d4fc6ffa7e\\scripts' : "output",
     platform: 'node',
 }).catch((e) => console.error(e));
